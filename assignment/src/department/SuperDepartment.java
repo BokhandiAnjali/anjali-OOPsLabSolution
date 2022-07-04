3c2601719ce7108278b6f8550af741dc7369bbf8
BokhandiAnjali <107784504+BokhandiAnjali@users.noreply.github.com>
2022-07-04 05:45:24 +0200

    Add files via upload

---
+++ assignment/src/department/SuperDepartment.java
@@ -0,0 +1,18 @@
+package department;
+
+public class SuperDepartment {
+
+    public String  departmentName(){
+        return "Super Department";
+    }
+    public String  getTodaysWork(){
+        return "No Work as of now";
+    }
+    public String  getWorkDeadline(){
+        return "Nill";
+    }
+    public String  isTodayAHoliday() {
+        return "Today is not a holiday";
+    }
+
+}
